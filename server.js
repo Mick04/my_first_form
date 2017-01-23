4ba64b0c56d2fbe9601589344b8c2d5290887931
Mick04 <mac5y4@talktalk.net>
2017-01-23 15:12:44 +0000

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var http = require('http');
 var fs = require('fs');
+var formidable = require("formidable");
+var util = require('util');
+
 var server = http.createServer(function (req, res) {
-    displayForm(res);
+    if (req.method.toLowerCase() == 'get') {
+        displayForm(res);
+    } else if (req.method.toLowerCase() == 'post') {
+        processAllFieldsOfTheForm(req, res);
+    }
+
 });
 
 function displayForm(res) {
@@ -15,5 +23,46 @@ function displayForm(res) {
     });
 }
 
+function processAllFieldsOfTheForm(req, res) {
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
+        //Store the data from the fields in your data store.
+        //The data store could be a file or database or any other store based
+        //on your application.
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+        res.write('received the data:\n\n');
+        res.end(util.inspect({
+            fields: fields,
+            files: files
+        }));
+    });
+}
+
+function processFormFieldsIndividual(req, res) {
+    //Store the data from the fields in your data store.
+    //The data store could be a file or database or any other store based
+    //on your application.
+    var fields = [];
+    var form = new formidable.IncomingForm();
+    form.on('field', function (field, value) {
+        console.log(field);
+        console.log(value);
+        fields[field] = value;
+    });
+
+    form.on('end', function () {
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+        res.write('received the data:\n\n');
+        res.end(util.inspect({
+            fields: fields
+        }));
+    });
+    form.parse(req);
+}
 server.listen(1185);
 console.log("server listening on 1185");
